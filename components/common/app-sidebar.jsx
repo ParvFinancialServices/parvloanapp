e6216b5e296ce4989dadb7a82daefc2a40e5b748
Abhishek Kumar <abhi24033c@gmail.com>
2025-06-08 22:04:59 +0530

    Dashbioard added

---
+++ components/common/app-sidebar.jsx
@@ -0,0 +1,63 @@
+"use client";
+
+
+import { useUserState } from "@/app/dashboard/store";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+
+import { usePathname } from "next/navigation";
+import { NavMain } from "./nav_main";
+import { NavProjects } from "./nav-projects";
+import { NavUser } from "./nav-user";
+// import { AdminSidebar } from "@/lib/AdminSideBar";
+import { AdminSidebar, DSASidebar, FieldStaffSidebar, RMSidebar, TelecallerSidebar } from "@/lib/Sidebarconfig";
+
+export function AppSidebar({ ...props }) {
+  const userState = useUserState();
+  const pathname = usePathname();
+  let roleData = {};
+
+  switch (userState.profile?.role) {
+    case "Admin":
+      roleData = AdminSidebar;
+      break;
+    case "rm":
+      roleData = RMSidebar;
+      break;
+    case "DSA":
+      roleData = DSASidebar;
+      break;
+    case "Telecaller":
+      roleData = TelecallerSidebar;
+      break;
+    case"Field Staff":
+      roleData = FieldStaffSidebar;
+      break;
+    // default:
+    //   roleData = FieldStaffSidebar;
+    //   break;
+  }
+
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <div className="w-full flex flex-row items-center justify-start p-2">
+        <div className="gap-2 flex flex-row justify-start items-center overflow-hidden w-fit">
+          <img src="/logo/PAR2.png" alt="Logo" className="w-10 aspect-square" />
+          <h2 className="min-w-max">Parv Financial Service</h2>
+        </div>
+      </div>
+      <SidebarContent>
+        <NavMain items={roleData?.navMain} pathname={pathname} />
+        <NavProjects projects={roleData?.projects} pathname={pathname} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={userState?.profile} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
